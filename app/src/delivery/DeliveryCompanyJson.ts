3521c1337b5d5f50955231a7b8e1f3e1c46cb4b5
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-03-06 15:38:59 -0500

    created DeliveryCompanyJson

---
+++ app/src/delivery/DeliveryCompanyJson.ts
@@ -0,0 +1,27 @@
+export class DeliveryCompanyJson {
+    private readonly id: number;
+    private readonly name: string;
+
+    constructor(
+        id: number,
+        name: string,
+    ) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public static from(body: any): DeliveryCompanyJson {
+        return new DeliveryCompanyJson(
+            body.id,
+            body.name
+        )
+    }
+}
