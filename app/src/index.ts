ba5766014376e075ec8b49c76d168a63240218b4
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-03-06 16:17:25 -0500

    exported app for testing

--- app/src/index.ts
+++ app/src/index.ts
@@ -3,11 +3,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import router from "./resources";
 
-
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -17,3 +16,5 @@ app.use("/api", router);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+export { app }; // Export for testing
