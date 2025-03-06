e5e0bef2d90d1d87efe7f9e946bb119a8a789582
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-03-05 19:58:41 -0500

    moved all boiler plate code to customer resource

--- app/src/customer/CustomerResource.ts
+++ app/src/customer/CustomerResource.ts
@@ -1,15 +1,51 @@
-import express from "express";
-import {addCustomer, getCustomerById, deleteCustomer, getCustomers, updateCustomer} from "./CustomerService";
+import express, {Request, Response} from "express";
+import { CustomerService } from "./CustomerService";
+import {CustomerJson} from "./CustomerJson";
 
 const router = express.Router();
+const customerService = new CustomerService();
 
-router.get("/", getCustomers);
-router.get("/:id", getCustomerById);
+router.get("/", async (req: Request, res: Response) => {
+    res
+        .status(200)
+        .json(
+            await customerService.get()
+        );
+});
 
-router.post("/", addCustomer);
+router.get("/:id", async (req: Request, res: Response) => {
+    res
+        .status(200)
+        .json(
+            await customerService.getById(
+                Number(req.params.id)
+            )
+        );
+});
 
-router.put("/:id", updateCustomer);
+router.post("/", async  (req: Request, res: Response) => {
+    res
+        .status(201)
+        .json(
+            await customerService.add(
+                CustomerJson.from(req.body)
+            )
+        );
+});
 
-router.delete("/:id", deleteCustomer);
+router.put("/:id", async (req: Request, res: Response) => {    res
+        .status(200)
+        .json(
+            await customerService.update(
+                Number(req.params.id),
+                CustomerJson.from(req.body)
+            )
+        );
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+    await customerService.delete(Number(req.params.id));
+    res.status(204).send();
+});
 
 export default router;
