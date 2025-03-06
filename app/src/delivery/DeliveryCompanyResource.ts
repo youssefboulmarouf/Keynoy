7ff11e19ed528a5ff10ed2816eb8f902906ba6fe
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-03-06 15:39:25 -0500

    moved the boiler plate code to resource

--- app/src/delivery/DeliveryCompanyResource.ts
+++ app/src/delivery/DeliveryCompanyResource.ts
@@ -1,15 +1,55 @@
-import express from "express";
-import {addDeliveryCompany, getDeliveryCompanies, updateDeliveryCompany, getDeliveryCompanyById, deleteDeliveryCompany} from "./DeliveryCompanyService";
+import express, {Request, Response} from "express";
+import { DeliveryCompanyService } from "./DeliveryCompanyService";
+import {CustomerJson} from "../customer/CustomerJson";
+import {DeliveryCompanyJson} from "./DeliveryCompanyJson";
 
 const router = express.Router();
+const deliveryCompanyService = new DeliveryCompanyService();
 
-router.get("/", getDeliveryCompanies);
-router.get("/:id", getDeliveryCompanyById);
+router.get("/", async (req: Request, res: Response) => {
+    res
+        .status(200)
+        .json(
+            await deliveryCompanyService.get()
+        );
+});
 
-router.post("/", addDeliveryCompany);
+router.get("/:id", async (req: Request, res: Response) => {
+    res
+        .status(200)
+        .json(
+            await deliveryCompanyService.getById(
+                Number(req.params.id)
+            )
+        );
+});
 
-router.put("/:id", updateDeliveryCompany);
 
-router.delete("/:id", deleteDeliveryCompany);
+router.post("/", async  (req: Request, res: Response) => {
+    res
+        .status(201)
+        .json(
+            await deliveryCompanyService.add(
+                DeliveryCompanyJson.from(req.body)
+            )
+        );
+});
+
+router.put("/:id", async (req: Request, res: Response) => {    res
+    .status(200)
+    .json(
+        await deliveryCompanyService.update(
+            Number(req.params.id),
+            CustomerJson.from(req.body)
+        )
+    );
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+    await deliveryCompanyService.delete(Number(req.params.id));
+    res.status(204).send();
+});
+
+
 
 export default router;
