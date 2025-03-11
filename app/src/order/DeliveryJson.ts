114c6eeda27fdb30c2d95be7500f31201594e6cb
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-03-11 16:34:27 -0400

    created delivery json

---
+++ app/src/order/DeliveryJson.ts
@@ -0,0 +1,51 @@
+export class DeliveryJson {
+    private readonly orderId: number;
+    private readonly dcId: number;
+    private readonly shippingDate: Date;
+    private readonly deliveryDate: Date;
+    private readonly price: number;
+
+    constructor(
+        orderId: number,
+        dcId: number,
+        shippingDate: Date,
+        deliveryDate: Date,
+        price: number
+    ) {
+        this.orderId = orderId;
+        this.dcId = dcId;
+        this.shippingDate = shippingDate;
+        this.deliveryDate = deliveryDate;
+        this.price = price;
+    }
+
+    public getOrderId(): number {
+        return this.orderId;
+    }
+
+    public getDcId(): number {
+        return this.dcId;
+    }
+
+    public getShippingDate(): Date {
+        return this.shippingDate;
+    }
+
+    public getDeliveryDate(): Date {
+        return this.deliveryDate;
+    }
+
+    public getPrice(): number {
+        return this.price;
+    }
+
+    public static from(body: any): DeliveryJson {
+        return new DeliveryJson(
+            Number(body.orderId),
+            Number(body.dcId),
+            body.shippingDate,
+            body.deliveryDate,
+            Number(body.price)
+        )
+    }
+}
