9048cf8373be3b36e9e3a11eacc035ff16f5e7f5
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-03-12 18:04:31 -0400

    introduced logger in the async handler

--- app/src/utilities/HandleAsync.ts
+++ app/src/utilities/HandleAsync.ts
@@ -1,22 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import AppError from "./AppError";
+import Logger from "./Logger";
+
+const logger = new Logger("HandleAsync");
 
 const handleAsync =
     (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
         async (req: Request, res: Response, next: NextFunction) => {
             try {
                 await fn(req, res, next);
-            } catch (e) {
+            } catch (e: any) {
                 if (e instanceof AppError) {
+                    logger.error(e.message, e);
                     res.status(e.status).json({
                         name: e.name,
                         message: e.message,
                     });
                 } else {
-                    console.error("Unexpected error:", e);
+                    logger.error("Unexpected error:", e);
                     res.status(500).json({
                         name: "UnknownError",
-                        message: "An unexpected error occurred.",
+                        message: `An unexpected error occurred: ${e.message}`,
                     });
                 }
             }
