284b6a22df823075fd8be48c6c731b8decb1c334
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-03-12 18:54:30 -0400

    added logging and async handling to delivery company module

--- app/src/delivery/DeliveryCompanyResource.ts
+++ app/src/delivery/DeliveryCompanyResource.ts
@@ -1,53 +1,73 @@
 import express, {Request, Response} from "express";
 import { DeliveryCompanyService } from "./DeliveryCompanyService";
 import {DeliveryCompanyJson} from "./DeliveryCompanyJson";
+import handleAsync from "../utilities/HandleAsync";
 
 const router = express.Router();
 const deliveryCompanyService = new DeliveryCompanyService();
 
-router.get("/", async (req: Request, res: Response) => {
-    res
-        .status(200)
-        .json(
-            await deliveryCompanyService.get()
-        );
-});
+router.get("/",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+                .status(200)
+                .json(
+                    await deliveryCompanyService.get()
+                );
+        }
+    )
+);
 
-router.get("/:id", async (req: Request, res: Response) => {
-    res
-        .status(200)
-        .json(
-            await deliveryCompanyService.getById(
-                Number(req.params.id)
-            )
-        );
-});
+router.get("/:id",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+                .status(200)
+                .json(
+                    await deliveryCompanyService.getById(
+                        Number(req.params.id)
+                    )
+                );
+        }
+    )
+);
 
-router.post("/", async  (req: Request, res: Response) => {
-    res
-        .status(201)
-        .json(
-            await deliveryCompanyService.add(
-                DeliveryCompanyJson.from(req.body)
-            )
-        );
-});
+router.post("/",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+                .status(201)
+                .json(
+                    await deliveryCompanyService.add(
+                        DeliveryCompanyJson.from(req.body)
+                    )
+                );
+        }
+    )
+);
 
-router.put("/:id", async (req: Request, res: Response) => {    res
-    .status(200)
-    .json(
-        await deliveryCompanyService.update(
-            Number(req.params.id),
-            DeliveryCompanyJson.from(req.body)
-        )
-    );
-});
+router.put("/:id",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+                .status(200)
+                .json(
+                    await deliveryCompanyService.update(
+                        Number(req.params.id),
+                        DeliveryCompanyJson.from(req.body)
+                    )
+                );
+        }
+    )
+);
 
-router.delete("/:id", async (req: Request, res: Response) => {
-    await deliveryCompanyService.delete(Number(req.params.id));
-    res.status(204).send();
-});
-
-
+router.delete("/:id",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            await deliveryCompanyService.delete(Number(req.params.id));
+            res.status(204).send();
+        }
+    )
+);
 
 export default router;
--- app/src/delivery/DeliveryCompanyService.ts
+++ app/src/delivery/DeliveryCompanyService.ts
@@ -1,23 +1,34 @@
 import {BaseService} from "../utilities/BaseService";
 import {DeliveryCompanyJson} from "./DeliveryCompanyJson";
+import AppError from "../utilities/AppError";
 
 export class DeliveryCompanyService extends BaseService {
     constructor() {
-        super();
+        super(DeliveryCompanyService.name);
     }
 
     async get(): Promise<DeliveryCompanyJson[]> {
+        this.logger.log(`Get all delivery companies`);
         const data = await this.prisma.deliveryCompany.findMany();
         return data.map((c: any) => DeliveryCompanyJson.from(c));
     }
 
     async getById(deliveryCompanyId: number): Promise<DeliveryCompanyJson> {
-        return DeliveryCompanyJson.from(
-            await this.prisma.deliveryCompany.findUnique({ where: { id: deliveryCompanyId } })
-        );
+        this.logger.log(`Get all delivery company by [id: ${deliveryCompanyId}]`);
+
+        const dcData = await this.prisma.deliveryCompany.findUnique({
+            where: { id: deliveryCompanyId }
+        });
+
+        if (!dcData) {
+            throw new AppError("Not Found", 404, `Delivery Company with [id:${deliveryCompanyId}] not found`);
+        }
+
+        return DeliveryCompanyJson.from(dcData);
     }
 
     async add(deliveryCompany: DeliveryCompanyJson): Promise<DeliveryCompanyJson> {
+        this.logger.log(`Create new delivery company`, deliveryCompany);
         return DeliveryCompanyJson.from(
             await this.prisma.deliveryCompany.create({
                 data: {
@@ -28,6 +39,17 @@ export class DeliveryCompanyService extends BaseService {
     }
 
     async update(deliveryCompanyId: number, deliveryCompany: any): Promise<DeliveryCompanyJson> {
+        this.logger.log(`Update delivery company by [id: ${deliveryCompanyId}]`);
+
+        if (deliveryCompanyId != deliveryCompany.getId()) {
+            throw new AppError("Bad Request", 400, `Delivery Company id mismatch`);
+        }
+
+        const existingDc = this.getById(deliveryCompanyId);
+
+        this.logger.log(`Update existing delivery company`, existingDc);
+        this.logger.log(`Delivery company updated data`, deliveryCompany);
+
         return DeliveryCompanyJson.from(
             await this.prisma.deliveryCompany.update({
                 where: { id: deliveryCompanyId },
@@ -39,8 +61,11 @@ export class DeliveryCompanyService extends BaseService {
     }
 
     async delete(deliveryCompanyId: number): Promise<void> {
+        this.logger.log(`Delete delivery company with [id=${deliveryCompanyId}]`);
+
         await this.prisma.deliveryCompany.delete({
             where: { id: deliveryCompanyId }
-        });    }
+        });
+    }
 
 }
