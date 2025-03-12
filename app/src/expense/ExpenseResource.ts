fec4acff4dbd77cb5c02e56bc217896501ca1732
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-03-12 19:10:19 -0400

    introduced logger to expense module

--- app/src/expense/ExpenseResource.ts
+++ app/src/expense/ExpenseResource.ts
@@ -1,52 +1,73 @@
 import express, {Request, Response} from "express";
 import {ExpenseJson} from "./ExpenseJson";
 import {ExpenseService} from "./ExpenseService";
+import handleAsync from "../utilities/HandleAsync";
 
 const router = express.Router();
 const expenseService = new ExpenseService();
 
-router.get("/", async (req: Request, res: Response) => {
-    res
-        .status(200)
-        .json(
-            await expenseService.get()
-        );
-});
+router.get("/",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+                .status(200)
+                .json(
+                    await expenseService.get()
+                );
+        }
+    )
+);
 
-router.get("/:id", async (req: Request, res: Response) => {
-    res
-        .status(200)
-        .json(
-            await expenseService.getById(
-                Number(req.params.id)
-            )
-        );
-});
+router.get("/:id",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+                .status(200)
+                .json(
+                    await expenseService.getById(
+                        Number(req.params.id)
+                    )
+                );
+        }
+    )
+);
 
-router.post("/", async  (req: Request, res: Response) => {
-    res
-        .status(201)
-        .json(
-            await expenseService.add(
-                ExpenseJson.from(req.body)
-            )
-        );
-});
+router.post("/",
+    handleAsync(
+        async  (req: Request, res: Response) => {
+            res
+                .status(201)
+                .json(
+                    await expenseService.add(
+                        ExpenseJson.from(req.body)
+                    )
+                );
+        }
+    )
+);
 
-router.put("/:id", async (req: Request, res: Response) => {
-    res
-        .status(200)
-        .json(
-            await expenseService.update(
-                Number(req.params.id),
-                ExpenseJson.from(req.body)
-            )
-        );
-});
+router.put("/:id",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+                .status(200)
+                .json(
+                    await expenseService.update(
+                        Number(req.params.id),
+                        ExpenseJson.from(req.body)
+                    )
+                );
+        }
+    )
+);
 
-router.delete("/:id", async (req: Request, res: Response) => {
-    await expenseService.delete(Number(req.params.id));
-    res.status(204).send();
-});
+router.delete("/:id",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            await expenseService.delete(Number(req.params.id));
+            res.status(204).send();
+        }
+    )
+);
 
 export default router;
--- app/src/expense/ExpenseService.ts
+++ app/src/expense/ExpenseService.ts
@@ -1,25 +1,34 @@
 import {BaseService} from "../utilities/BaseService";
 import {ExpenseJson} from "./ExpenseJson";
+import AppError from "../utilities/AppError";
 
 export class ExpenseService extends BaseService {
     constructor() {
-        super();
+        super(ExpenseService.name);
     }
 
     async get(): Promise<ExpenseJson[]> {
+        this.logger.log(`Get all expenses`);
         const data = await this.prisma.expense.findMany();
         return data.map((c: any) => ExpenseJson.from(c));
     };
 
     async getById(expenseId: number): Promise<ExpenseJson> {
-        return ExpenseJson.from(
-            await this.prisma.expense.findUnique({
-                    where: { id: expenseId }
-            })
-        );
+        this.logger.log(`Get expense by [id:${expenseId}]`);
+
+        const expenseData = await this.prisma.expense.findUnique({
+            where: { id: expenseId }
+        });
+
+        if (!expenseData) {
+            throw new AppError("Not Found", 404, `Expense with [id:${expenseId}] not found`);
+        }
+
+        return ExpenseJson.from(expenseData);
     };
 
     async add(expense: ExpenseJson): Promise<ExpenseJson> {
+        this.logger.log(`Create new expense`, expense);
         return ExpenseJson.from(
             await this.prisma.expense.create({
                 data: {
@@ -34,6 +43,17 @@ export class ExpenseService extends BaseService {
     };
 
     async update(expenseId: number, expense: any): Promise<ExpenseJson> {
+        this.logger.log(`Update expense with [id=${expenseId}]`);
+
+        if (expenseId != expense.getId()) {
+            throw new AppError("Bad Request", 400, `Expense id mismatch`);
+        }
+
+        const existingExpense = this.getById(expenseId);
+
+        this.logger.log(`Update existing expense`, existingExpense);
+        this.logger.log(`Expense updated data`, expense);
+
         return ExpenseJson.from(
             await this.prisma.expense.update({
                 where: { id: expenseId },
@@ -49,12 +69,14 @@ export class ExpenseService extends BaseService {
     }
 
     async delete(expenseId: number): Promise<void> {
+        this.logger.log(`Delete expense with [id=${expenseId}]`);
         await this.prisma.expense.delete({
             where: { id: expenseId }
         });
     }
 
     async deleteByOrderId(orderId: number): Promise<void> {
+        this.logger.log(`Delete expense with [orderId=${orderId}]`);
         await this.prisma.expense.deleteMany({
             where: { orderId: orderId }
         });
