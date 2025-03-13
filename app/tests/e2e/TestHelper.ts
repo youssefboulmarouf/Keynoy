11d489a8500631a9b1fe1a70639bc37454726e2f
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-03-12 22:05:06 -0400

    added methods to heper

--- app/tests/e2e/TestHelper.ts
+++ app/tests/e2e/TestHelper.ts
@@ -1,10 +1,18 @@
 import request from "supertest";
 import {app} from "../../src";
 
+export async function getEntity(path: string): Promise<any> {
+    return await request(app).get(path);
+}
+
 export async function createEntity(path: string, entity: any): Promise<any> {
     return await request(app).post(path).send(entity);
 }
 
 export async function updateEntity(path: string, entity: any): Promise<any> {
-    return await request(app).put(path + "/" + entity.id).send(entity);
+    return await request(app).put(path).send(entity);
+}
+
+export async function deleteEntity(path: string): Promise<any> {
+    return await request(app).delete(path);
 }
