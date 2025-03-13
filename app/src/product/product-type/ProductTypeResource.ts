e35c2c0117c88cb82b00acb7e417d60fb02f4aea
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-03-12 21:12:11 -0400

    introduced logger to product types

--- app/src/product/product-type/ProductTypeResource.ts
+++ app/src/product/product-type/ProductTypeResource.ts
@@ -1,53 +1,73 @@
 import express, {Request, Response} from "express";
 import { ProductTypeService } from "./ProductTypeService";
 import {ProductTypeJson} from "./ProductTypeJson";
+import handleAsync from "../../utilities/HandleAsync";
 
 const router = express.Router();
 const productTypeService = new ProductTypeService();
 
-router.get("/", async (req: Request, res: Response) => {
-    res
-        .status(200)
-        .json(
-            await productTypeService.get()
-        );
-});
+router.get("/",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+                .status(200)
+                .json(
+                    await productTypeService.get()
+                );
+        }
+    )
+);
 
-router.get("/:id", async (req: Request, res: Response) => {
-    res
-        .status(200)
-        .json(
-            await productTypeService.getById(
-                Number(req.params.id)
-            )
-        );
-});
+router.get("/:id",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+                .status(200)
+                .json(
+                    await productTypeService.getById(
+                        Number(req.params.id)
+                    )
+                );
+        }
+    )
+);
 
-router.post("/", async  (req: Request, res: Response) => {
-    res
-        .status(201)
-        .json(
-            await productTypeService.add(
-                ProductTypeJson.from(req.body)
-            )
-        );
-});
+router.post("/",
+    handleAsync(
+        async  (req: Request, res: Response) => {
+            res
+                .status(201)
+                .json(
+                    await productTypeService.add(
+                        ProductTypeJson.from(req.body)
+                    )
+                );
+        }
+    )
+);
 
-router.put("/:id", async (req: Request, res: Response) => {    res
-    .status(200)
-    .json(
-        await productTypeService.update(
-            Number(req.params.id),
-            ProductTypeJson.from(req.body)
-        )
-    );
-});
+router.put("/:id",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+            .status(200)
+            .json(
+                await productTypeService.update(
+                    Number(req.params.id),
+                    ProductTypeJson.from(req.body)
+                )
+            );
+        }
+    )
+);
 
-router.delete("/:id", async (req: Request, res: Response) => {
-    await productTypeService.delete(Number(req.params.id));
-    res.status(204).send();
-});
-
-
+router.delete("/:id",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            await productTypeService.delete(Number(req.params.id));
+            res.status(204).send();
+        }
+    )
+);
 
 export default router;
--- app/src/product/product-type/ProductTypeService.ts
+++ app/src/product/product-type/ProductTypeService.ts
@@ -1,23 +1,34 @@
 import {BaseService} from "../../utilities/BaseService";
 import {ProductTypeJson} from "./ProductTypeJson";
+import AppError from "../../utilities/AppError";
 
 export class ProductTypeService extends BaseService {
     constructor() {
-        super();
+        super(ProductTypeService.name);
     }
 
     async get(): Promise<ProductTypeJson[]> {
+        this.logger.log(`Get all product types`);
         const data = await this.prisma.productType.findMany();
         return data.map((c: any) => ProductTypeJson.from(c));
     }
 
     async getById(productTypeId: number): Promise<ProductTypeJson> {
-        return ProductTypeJson.from(
-            await this.prisma.productType.findUnique({ where: { id: productTypeId } })
-        );
+        this.logger.log(`Get product type by [id:${productTypeId}]`);
+
+        const productTypeData = await this.prisma.productType.findUnique({
+            where: { id: productTypeId }
+        });
+
+        if (!productTypeId) {
+            throw new AppError("Not Found", 404, `Product type with [id:${productTypeId}] not found`);
+        }
+
+        return ProductTypeJson.from(productTypeData);
     }
 
     async add(productType: ProductTypeJson): Promise<ProductTypeJson> {
+        this.logger.log(`Create new product type`, productType);
         return ProductTypeJson.from(
             await this.prisma.productType.create({
                 data: {
@@ -28,6 +39,17 @@ export class ProductTypeService extends BaseService {
     }
 
     async update(productTypeId: number, productType: any): Promise<ProductTypeJson> {
+        this.logger.log(`Update product type with [id=${productTypeId}]`);
+
+        if (productTypeId != productType.getId()) {
+            throw new AppError("Bad Request", 400, `Product type id mismatch`);
+        }
+
+        const existingPt = this.getById(productTypeId);
+
+        this.logger.log(`Update existing product type`, existingPt);
+        this.logger.log(`Product type updated data`, productType);
+
         return ProductTypeJson.from(
             await this.prisma.productType.update({
                 where: { id: productTypeId },
@@ -39,6 +61,8 @@ export class ProductTypeService extends BaseService {
     }
 
     async delete(productTypeId: number): Promise<void> {
+        this.logger.log(`Delete product type with [id=${productTypeId}]`);
+
         await this.prisma.productType.delete({
             where: { id: productTypeId }
         });
