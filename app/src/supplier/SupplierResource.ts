e81e1b9645d7eee5236def5e01f7e1cfa4fe5a8b
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-03-12 21:23:08 -0400

    introduced logger to supplier

--- app/src/supplier/SupplierResource.ts
+++ app/src/supplier/SupplierResource.ts
@@ -1,52 +1,74 @@
 import express, {Request, Response} from "express";
 import { SupplierService } from "./SupplierService";
 import {SupplierJson} from "./SupplierJson";
+import handleAsync from "../utilities/HandleAsync";
 
 const router = express.Router();
 const supplierService = new SupplierService();
 
-router.get("/", async (req: Request, res: Response) => {
-    res
-        .status(200)
-        .json(
-            await supplierService.get()
-        );
-});
+router.get("/",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+                .status(200)
+                .json(
+                    await supplierService.get()
+                );
+        }
+    )
+);
 
-router.get("/:id", async (req: Request, res: Response) => {
-    res
-        .status(200)
-        .json(
-            await supplierService.getById(
-                Number(req.params.id)
-            )
-        );
-});
+router.get("/:id",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+                .status(200)
+                .json(
+                    await supplierService.getById(
+                        Number(req.params.id)
+                    )
+                );
+        }
+    )
+);
 
-router.post("/", async  (req: Request, res: Response) => {
-    res
-        .status(201)
-        .json(
-            await supplierService.add(
-                SupplierJson.from(req.body)
-            )
-        );
-});
+router.post("/",
+    handleAsync(
+        async  (req: Request, res: Response) => {
+            res
+                .status(201)
+                .json(
+                    await supplierService.add(
+                        SupplierJson.from(req.body)
+                    )
+                );
+        }
+    )
+);
 
-router.put("/:id", async (req: Request, res: Response) => {    res
-    .status(200)
-    .json(
-        await supplierService.update(
-            Number(req.params.id),
-            SupplierJson.from(req.body)
-        )
-    );
-});
+router.put("/:id",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            res
+                .status(200)
+                .json(
+                    await supplierService.update(
+                        Number(req.params.id),
+                        SupplierJson.from(req.body)
+                    )
+                );
+        }
+    )
+);
 
-router.delete("/:id", async (req: Request, res: Response) => {
-    await supplierService.delete(Number(req.params.id));
-    res.status(204).send();
-});
+router.delete("/:id",
+    handleAsync(
+        async (req: Request, res: Response) => {
+            await supplierService.delete(Number(req.params.id));
+            res.status(204).send();
+        }
+    )
+);
 
 
 
--- app/src/supplier/SupplierService.ts
+++ app/src/supplier/SupplierService.ts
@@ -1,23 +1,34 @@
 import {BaseService} from "../utilities/BaseService";
 import {SupplierJson} from "./SupplierJson";
+import AppError from "../utilities/AppError";
 
 export class SupplierService extends BaseService {
     constructor() {
-        super();
+        super(SupplierService.name);
     }
 
     async get(): Promise<SupplierJson[]> {
+        this.logger.log(`Get all suppliers`);
         const data = await this.prisma.supplier.findMany();
         return data.map((c: any) => SupplierJson.from(c));
     }
 
     async getById(supplierId: number): Promise<SupplierJson> {
-        return SupplierJson.from(
-            await this.prisma.supplier.findUnique({ where: { id: supplierId } })
-        );
+        this.logger.log(`Get supplier by [id:${supplierId}]`);
+
+        const supplierData = await this.prisma.supplier.findUnique({
+            where: { id: supplierId }
+        });
+
+        if (!supplierData) {
+            throw new AppError("Not Found", 404, `Supplier with [id:${supplierId}] not found`);
+        }
+
+        return SupplierJson.from(supplierData);
     }
 
     async add(supplier: SupplierJson): Promise<SupplierJson> {
+        this.logger.log(`Create supplier customer`, supplier);
         return SupplierJson.from(
             await this.prisma.supplier.create({
                 data: {
@@ -28,6 +39,17 @@ export class SupplierService extends BaseService {
     }
 
     async update(supplierId: number, supplier: any): Promise<SupplierJson> {
+        this.logger.log(`Update supplier with [id=${supplierId}]`);
+
+        if (supplierId != supplier.getId()) {
+            throw new AppError("Bad Request", 400, `Supplier id mismatch`);
+        }
+
+        const existingSupplier = this.getById(supplierId);
+
+        this.logger.log(`Update existing supplier`, existingSupplier);
+        this.logger.log(`Supplier updated data`, supplier);
+
         return SupplierJson.from(
             await this.prisma.supplier.update({
                 where: { id: supplierId },
@@ -39,6 +61,8 @@ export class SupplierService extends BaseService {
     }
 
     async delete(supplierId: number): Promise<void> {
+        this.logger.log(`Delete supplier with [id=${supplierId}]`);
+
         await this.prisma.supplier.delete({
             where: { id: supplierId }
         });
