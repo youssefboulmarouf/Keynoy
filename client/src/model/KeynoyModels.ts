dd413dd3654a3766abc853033bf6d52f8c1fb747
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-04-07 18:36:38 -0400

    added more models

--- client/src/model/KeynoyModels.ts
+++ client/src/model/KeynoyModels.ts
@@ -1,4 +1,89 @@
+export type CompanyJson = {
+    id: number;
+    name: string;
+    type: string;
+    phone: string;
+    location: string;
+}
+
+export type ExpenseJson = {
+    id: number;
+    name: string;
+    totalPrice: number;
+    date: Date;
+    orderId: number;
+}
+
+export type OrderLineJson = {
+    orderId: number;
+    productId: number;
+    quantity: number;
+    unitPrice: number;
+}
+
+export enum OrderStatusEnum {
+    CONFIRMED = 1,
+    IN_PROGRESS = 2,
+    FINISHED = 3,
+    SHIPPED = 4,
+    DELIVERED = 5,
+    RETURNED = 6,
+}
+
+export enum OrderTypeEnum {
+    BUY = "buy",
+    SELL = "sell",
+}
+
+export type OrderJson = {
+    id: number;
+    customerId: number;
+    supplierId: number;
+    orderType: OrderTypeEnum;
+    orderStatus: OrderStatusEnum;
+    totalPrice: number;
+    date: Date;
+    orderLines: OrderLineJson[];
+}
+
 export type ProductTypeJson = {
     id: number;
     name: string;
 };
+
+export enum ColorEnum {
+    TRANSPARENT = "transparent",
+    RED = "red",
+    BLUE = "blue",
+    GREEN = "green",
+    YELLOW = "yellow",
+    ORANGE = "orange",
+    PURPLE = "purple",
+    BLACK = "black",
+    WHITE = "white",
+    GRAY = "gray",
+    PINK = "pink",
+    BROWN = "brown",
+    CYAN = "cyan",
+    MAGENTA = "magenta",
+    GOLD = "gold",
+    UNKNOWN = "unknown",
+}
+
+export type ProductJson = {
+    id: number;
+    name: string;
+    size: string;
+    productTypeId: number;
+    color: ColorEnum;
+    threshold: number;
+    totalQuantity: number;
+}
+
+export type ShippingJson = {
+    orderId: number;
+    shipperId: number;
+    shippingDate: Date;
+    deliveryDate: Date | null;
+    price: number;
+}
