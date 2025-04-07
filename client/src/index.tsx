cf95734c344fb8fad0e8e1a006a2e8be2f85bfae
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-04-07 18:31:21 -0400

    added react-query

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,15 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
     </StrictMode>,
 );
