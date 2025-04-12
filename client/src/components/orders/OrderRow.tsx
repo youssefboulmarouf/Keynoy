aedd1a7b251fcf33eae386db7d661e6990274aab
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-04-12 14:15:47 -0400

    added order row

---
+++ client/src/components/orders/OrderRow.tsx
@@ -0,0 +1,114 @@
+import {TableCell, TableRow, Tooltip} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import OrderStatusChip from "./OrderStatusChip";
+import OrderLinesTable from "./OrderLinesTable";
+import React from "react";
+import {ModalTypeEnum, OrderJson} from "../../model/KeynoyModels";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+interface OrderRowProps {
+    order: OrderJson;
+    rowIndex: number;
+    type: string;
+    openRow: boolean;
+    rowToOpen: number;
+    handleExpandRow: (index: number) => void;
+    getCompanyNameFromOrder: (order: OrderJson, companyType: string) => string;
+    getCompanyPhoneFromOrder: (order: OrderJson, companyType: string) => string;
+    productsData: any[];
+    productTypesData: any[];
+    handleOpenDialogType: (type: ModalTypeEnum, order: OrderJson) => void;
+}
+
+const OrderRow: React.FC<OrderRowProps> = ({
+    order,
+    rowIndex,
+    type,
+    openRow,
+    rowToOpen,
+    handleExpandRow,
+    getCompanyNameFromOrder,
+    getCompanyPhoneFromOrder,
+    productsData,
+    productTypesData,
+    handleOpenDialogType
+}) => {
+    const formatDate = (date: Date) => {
+        const newDate = new Date(date);
+        const day = newDate.getUTCDate().toString().padStart(2, "0");
+        const month = (newDate.getUTCMonth() + 1).toString().padStart(2, "0");
+        const year = newDate.getUTCFullYear();
+
+        return `${day}/${month}/${year}`;
+    };
+
+    return (
+        <>
+            <TableRow key={order.id}>
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => handleExpandRow(rowIndex)}
+                    >
+                        {openRow && rowToOpen === rowIndex ? (
+                            <KeyboardArrowUpIcon />
+                        ) : (
+                            <KeyboardArrowDownIcon />
+                        )}
+                    </IconButton>
+                </TableCell>
+                <TableCell>{order.id}</TableCell>
+                {type === "Achats" ? (
+                    <TableCell>
+                        {getCompanyNameFromOrder(order, "Fournisseurs") + " - " + getCompanyPhoneFromOrder(order, "Fournisseurs")}
+                    </TableCell>
+                ) : (
+                    <TableCell>
+                        {getCompanyNameFromOrder(order, "Clients") + " - " + getCompanyPhoneFromOrder(order, "Clients")}
+                    </TableCell>
+                )}
+                <TableCell>
+                    <OrderStatusChip orderStatus={order.orderStatus} />
+                </TableCell>
+                <TableCell>{order.totalPrice}</TableCell>
+                <TableCell>{formatDate(order.date)}</TableCell>
+                <TableCell align="right">
+                    <Tooltip title="Modifier Partenaire">
+                        <IconButton
+                            color="warning"
+                            onClick={() => {handleOpenDialogType(ModalTypeEnum.UPDATE, order)}}
+                        >
+                            <EditIcon width={22} />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Supprimer Partenaire">
+                        <IconButton
+                            color="error"
+                            onClick={() => {handleOpenDialogType(ModalTypeEnum.DELETE, order)}}
+                        >
+                            <DeleteIcon width={22} />
+                        </IconButton>
+                    </Tooltip>
+                </TableCell>
+            </TableRow>
+            <TableRow key={order.id+"line"}>
+                <TableCell sx={{ paddingBottom: 0, paddingTop: 0, backgroundColor: "primary.light"}} colSpan={12}>
+                    <OrderLinesTable
+                        openRow={openRow}
+                        rowToOpen={rowToOpen}
+                        rowIndex={rowIndex}
+                        orderLines={order.orderLines}
+                        products={productsData || []}
+                        productTypes={productTypesData || []}
+                    />
+                </TableCell>
+            </TableRow>
+        </>
+    )
+}
+
+export default OrderRow;
