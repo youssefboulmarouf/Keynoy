f09af1f0c91dc937bc6acbf6c6c01d185e8c6263
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-04-18 23:24:49 -0400

    added paint and color to OrderLineJson

--- app/src/order/OrderLineJson.ts
+++ app/src/order/OrderLineJson.ts
@@ -1,19 +1,28 @@
+import {PaintTypeEnum, paintTypeFromString} from "./PaintTypeEnum";
+import {ColorJson} from "../color/ColorJson";
+
 export class OrderLineJson {
     private orderId: number;
     private readonly productId: number;
     private readonly quantity: number;
     private readonly unitPrice: number;
+    private readonly paintType: PaintTypeEnum;
+    private readonly colors: ColorJson[];
 
     constructor(
         orderId: number,
         productId: number,
         quantity: number,
-        unitPrice: number
+        unitPrice: number,
+        paintType: PaintTypeEnum,
+        colors: ColorJson[]
     ) {
         this.orderId = orderId;
         this.productId = productId;
         this.quantity = quantity;
         this.unitPrice = unitPrice;
+        this.paintType = paintType;
+        this.colors = colors;
     }
 
     public setOrderId(orderId: number): void {
@@ -36,12 +45,33 @@ export class OrderLineJson {
         return this.unitPrice;
     }
 
-    public static from(body: any): OrderLineJson {
+    public getPaintType(): PaintTypeEnum {
+        return this.paintType;
+    }
+
+    public getColors(): ColorJson[] {
+        return this.colors;
+    }
+
+    public static fromObject(body: any): OrderLineJson {
         return new OrderLineJson(
             Number(body.orderId),
             Number(body.productId),
             Number(body.quantity),
             Number(body.unitPrice),
+            paintTypeFromString(body.paintType),
+            body.colors.map(ColorJson.from),
+        )
+    }
+
+    public static fromObjectAndColors(body: any, colors: ColorJson[]): OrderLineJson {
+        return new OrderLineJson(
+            Number(body.orderId),
+            Number(body.productId),
+            Number(body.quantity),
+            Number(body.unitPrice),
+            paintTypeFromString(body.paintType),
+            colors,
         )
     }
 }
--- app/src/order/OrderLineService.ts
+++ app/src/order/OrderLineService.ts
@@ -1,37 +1,52 @@
 import {BaseService} from "../utilities/BaseService";
 import {OrderLineJson} from "./OrderLineJson";
+import {ColorJson} from "../color/ColorJson";
+import {ColorService} from "../color/ColorService";
 
 export class OrderLineService extends BaseService {
+    private readonly colorService: ColorService;
+
     constructor() {
         super(OrderLineService.name);
+        this.colorService = new ColorService();
     }
 
     async get(): Promise<OrderLineJson[]> {
-        this.logger.log(`Get all order lines`);
-        const data = await this.prisma.orderLine.findMany();
-        return data.map((c: any) => OrderLineJson.from(c));
-    };
+        this.logger.log("Get all order lines");
+        const lines = await this.prisma.orderLine.findMany();
+        return await Promise.all(lines.map(async line =>
+            OrderLineJson.fromObjectAndColors(
+                line,
+                await this.colorService.getByOrderLineId(line.orderId, line.productId)
+            )
+        ));
+    }
 
     async getById(orderId: number): Promise<OrderLineJson[]> {
-        this.logger.log(`Get order line by [orderId:${orderId}]`);
-        const data = await this.prisma.orderLine.findMany({
-            where: { orderId: orderId }
-        });
-
-        return data.map((c: any) => OrderLineJson.from(c));
-    };
+        this.logger.log(`Get order lines by [orderId: ${orderId}]`);
+        const lines = await this.prisma.orderLine.findMany({where: { orderId }});
+        return await Promise.all(lines.map(async line =>
+            OrderLineJson.fromObjectAndColors(
+                line,
+                await this.colorService.getByOrderLineId(line.orderId, line.productId)
+            )
+        ));
+    }
 
     async add(orderLine: OrderLineJson): Promise<OrderLineJson> {
         this.logger.log(`Create new order line`, orderLine);
-        return OrderLineJson.from(
+
+        return OrderLineJson.fromObjectAndColors(
             await this.prisma.orderLine.create({
                 data: {
                     orderId: orderLine.getOrderId(),
                     productId: orderLine.getProductId(),
                     quantity: orderLine.getQuantity(),
                     unitPrice: orderLine.getUnitPrice(),
+                    paintType: orderLine.getPaintType(),
                 }
-            })
+            }),
+            await this.setOrderLineColors(orderLine.getOrderId(), orderLine.getProductId(), orderLine.getColors())
         );
     };
 
@@ -42,20 +57,19 @@ export class OrderLineService extends BaseService {
 
     async delete(orderId: number): Promise<void> {
         this.logger.log(`Delete all order lines by [orderId:${orderId}]`);
+        await this.prisma.orderLineColor.deleteMany({ where: { orderId} });
         await this.prisma.orderLine.deleteMany({
-            where: { orderId: orderId }
+            where: { orderId }
         });
     }
 
     async deleteUnique(orderId: number, productId: number): Promise<void> {
         this.logger.log(`Delete unique order line by [orderId:${orderId} && productId:${productId}]`);
+        await this.prisma.orderLineColor.deleteMany({ where: { orderId, productId } });
         await this.prisma.orderLine.delete({
             where: {
-                orderId_productId: {
-                    orderId,
-                    productId,
-                },
-            },
+                orderId_productId: { orderId, productId }
+            }
         });
     }
 
@@ -68,4 +82,21 @@ export class OrderLineService extends BaseService {
         }));
         return savedOrderLines;
     };
+
+    private async setOrderLineColors(orderId: number, productId: number, colors: ColorJson[]): Promise<ColorJson[]> {
+        await this.prisma.orderLineColor.deleteMany({ where: { orderId, productId } });
+
+        if (!colors?.length) return [];
+
+        await this.prisma.orderLineColor.createMany({
+            data: colors.map((c) => ({
+                orderId,
+                productId,
+                colorId: c.getId()
+            })),
+            skipDuplicates: true
+        });
+
+        return await this.colorService.getByOrderLineId(orderId, productId)
+    }
 }
