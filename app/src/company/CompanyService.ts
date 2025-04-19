a8640ea70f7f549843bb1e71a21395901881a8ca
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-04-18 23:22:40 -0400

    adjusted company service to consider designs

--- app/src/company/CompanyService.ts
+++ app/src/company/CompanyService.ts
@@ -3,50 +3,42 @@ import {CompanyTypeEnum} from "./CompanyTypeEnum";
 import {CompanyJson} from "./CompanyJson";
 import NotFoundError from "../utilities/errors/NotFoundError";
 import BadRequestError from "../utilities/errors/BadRequestError";
+import {CompanyDesignService} from "./CompanyDesignService";
+import {CompanyDesignJson} from "./CompanyDesignJson";
 
 export class CompanyService extends BaseService {
 
+    private readonly companyDesignService :CompanyDesignService;
+
     constructor() {
         super(CompanyService.name);
+        this.companyDesignService = new CompanyDesignService();
     }
 
     async get(): Promise<CompanyJson[]> {
-        this.logger.log(`Get all customers`);
-        const data = await this.prisma.company.findMany();
-        return data.map((c: any) => CompanyJson.from(c));
-    };
-
-    async getCustomers(): Promise<CompanyJson[]> {
-        this.logger.log(`Get all customers`);
-        return await this.getCompanies(CompanyTypeEnum.CUSTOMER);
-    };
-
-    async getSuppliers(): Promise<CompanyJson[]> {
-        this.logger.log(`Get all suppliers`);
-        return await this.getCompanies(CompanyTypeEnum.SUPPLIER);
-    };
-
-    async getShippers(): Promise<CompanyJson[]> {
-        this.logger.log(`Get all shippers`);
-        return await this.getCompanies(CompanyTypeEnum.SHIPPER);
-    };
-
-    private async getCompanies(type: string): Promise<CompanyJson[]> {
-        const data = await this.prisma.company.findMany({
-            where: { type: type }
+        this.logger.log(`Get all companies`);
+        const companiesData = await this.prisma.company.findMany({
+            include: { designs: true }
         });
-        return data.map((c: any) => CompanyJson.from(c));
+
+        return companiesData.map(c =>
+            CompanyJson.fromObjectAndDesigns(c, c.designs.map(CompanyDesignJson.from))
+        );
     };
 
-    async getById(companyId: number): Promise<CompanyJson> {
-        this.logger.log(`Get company by [id:${companyId}]`);
+    async getById(id: number): Promise<CompanyJson> {
+        this.logger.log(`Get company by [id:${id}]`);
         const companyData = await this.prisma.company.findUnique({
-            where: { id: companyId }
+            where: { id },
+            include: { designs: true }
         });
 
-        NotFoundError.throwIf(!companyData, `Customer with [id:${companyId}] not found`);
+        NotFoundError.throwIf(!companyData, `Company with [id:${id}] not found`);
 
-        return CompanyJson.from(companyData);
+        return CompanyJson.fromObjectAndDesigns(
+            companyData,
+            companyData?.designs.map(CompanyDesignJson.from) || []
+        );
     };
 
     async add(company: CompanyJson): Promise<CompanyJson> {
@@ -54,47 +46,56 @@ export class CompanyService extends BaseService {
 
         BadRequestError.throwIf(company.getType() === CompanyTypeEnum.UNKNOWN, `Company cannot be unknown.`);
 
-        return CompanyJson.from(
-            await this.prisma.company.create({
-                data: {
-                    name: company.getName(),
-                    type: company.getType(),
-                    phone: company.getPhone(),
-                    location: company.getLocation()
-                }
-            })
-        );
+        const companyData = await this.prisma.company.create({
+            data: {
+                name: company.getName(),
+                type: company.getType(),
+                phone: company.getPhone(),
+                location: company.getLocation()
+            }
+        });
+
+        this.logger.log(`Created company with [id: ${companyData.id}]`);
+
+        return CompanyJson.fromObjectAndDesigns(
+            companyData,
+            await this.companyDesignService.addList(company.getDesignUrls(), companyData.id)
+        )
     };
 
-    async update(companyId: number, company: CompanyJson): Promise<CompanyJson> {
-        this.logger.log(`Update company with [id=${companyId}]`);
+    async update(id: number, company: CompanyJson): Promise<CompanyJson> {
+        this.logger.log(`Update company with [id=${id}]`);
 
-        BadRequestError.throwIf(companyId != company.getId(), `Company id mismatch`);
+        BadRequestError.throwIf(id != company.getId(), `Company id mismatch`);
 
-        const existingCompany = this.getById(companyId);
+        const existingCompany = await this.getById(id);
 
         this.logger.log(`Update existing company`, existingCompany);
         this.logger.log(`Company updated data`, company);
 
-        return CompanyJson.from(
+        await this.companyDesignService.deleteByCompanyId(id);
+
+        return CompanyJson.fromObjectAndDesigns(
             await this.prisma.company.update({
-                where: { id: companyId },
+                where: { id },
                 data: {
                     name: company.getName(),
                     phone: company.getPhone(),
                     location: company.getLocation()
                 }
-            })
+            }),
+            await this.companyDesignService.addList(company.getDesignUrls(), id)
         );
     }
 
-    async delete(companyId: number): Promise<void> {
-        this.logger.log(`Delete company with [id=${companyId}]`);
+    async delete(id: number): Promise<void> {
+        this.logger.log(`Delete company with [id=${id}]`);
 
         // TODO: Avoid deleting company if have orders or shipping
 
+        await this.companyDesignService.deleteByCompanyId(id);
         await this.prisma.company.delete({
-            where: { id: companyId }
+            where: { id }
         });
     }
 }
