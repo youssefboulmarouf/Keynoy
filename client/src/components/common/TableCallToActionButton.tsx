7b872e137d0bae599fed177541ff6f8d8f9c4230
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-04-18 23:18:14 -0400

    added fullwidth to TableCallToActionButton

--- client/src/components/common/TableCallToActionButton.tsx
+++ client/src/components/common/TableCallToActionButton.tsx
@@ -3,12 +3,13 @@ import { Box, Button } from "@mui/material";
 interface TableCallToActionButtonProps {
     callToActionText: string;
     callToActionFunction: () => void;
+    fullwidth: boolean
 }
 
-const TableCallToActionButton: React.FC<TableCallToActionButtonProps> = ({callToActionText, callToActionFunction}) => {
+const TableCallToActionButton: React.FC<TableCallToActionButtonProps> = ({callToActionText, callToActionFunction, fullwidth}) => {
     return (
         <Box display="flex" gap={1}>
-            <Button variant="contained" color="primary" onClick={() => callToActionFunction()}>
+            <Button fullWidth={fullwidth} variant="contained" color="primary" onClick={() => callToActionFunction()}>
                 {callToActionText}
             </Button>
         </Box>
