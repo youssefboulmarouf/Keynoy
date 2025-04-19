723ba0416a68864a9a0baad701b9150e37905c88
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-04-18 23:34:05 -0400

    simplified OrderStatusChip

--- client/src/components/orders/OrderStatusChip.tsx
+++ client/src/components/orders/OrderStatusChip.tsx
@@ -1,35 +1,14 @@
 import {Chip} from "@mui/material";
 import React from "react";
 import {OrderStatusEnum} from "../../model/KeynoyModels";
+import {mapOrderStatusToColor, mapOrderStatusToString} from "./OrderStatusUtils";
 
 interface OrderStatusChipProps {
     orderStatus: OrderStatusEnum;
 }
 
 const OrderStatusChip: React.FC<OrderStatusChipProps> = ({orderStatus}) => {
-    const mapOrderStatusToString = (status: OrderStatusEnum) => {
-        switch (status) {
-            case OrderStatusEnum.CONFIRMED: return "Confirmer";
-            case OrderStatusEnum.IN_PROGRESS: return "En Cours";
-            case OrderStatusEnum.FINISHED: return "Terminer";
-            case OrderStatusEnum.SHIPPED: return "Livrer";
-            case OrderStatusEnum.DELIVERED: return "Recu";
-            case OrderStatusEnum.RETURNED: return "Retourner";
-        }
-    }
-
-    const mapOrderStatusToColor = (status: OrderStatusEnum) => {
-        switch (status) {
-            case OrderStatusEnum.CONFIRMED: return "primary";
-            case OrderStatusEnum.IN_PROGRESS: return "secondary";
-            case OrderStatusEnum.FINISHED: return "default";
-            case OrderStatusEnum.SHIPPED: return "warning";
-            case OrderStatusEnum.DELIVERED: return "success";
-            case OrderStatusEnum.RETURNED: return "error";
-        }
-    }
-
-    return (
+   return (
         <Chip
             color={mapOrderStatusToColor(orderStatus)}
             variant="outlined"
