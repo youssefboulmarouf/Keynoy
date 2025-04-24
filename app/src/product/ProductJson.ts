2c989a8e5cbcc203e00ee2b2c15119c83562997c
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-04-24 16:44:21 -0400

    adjusted product json to consider variation

--- app/src/product/ProductJson.ts
+++ app/src/product/ProductJson.ts
@@ -1,31 +1,21 @@
-import {ColorJson} from "../color/ColorJson";
+import {ProductVariationJson} from "./product-variation/ProductVariationJson";
 
 export class ProductJson {
     private readonly id: number;
     private readonly name: string;
-    private readonly size: string;
     private readonly productTypeId: number;
-    private readonly colors: ColorJson[];
-    private readonly threshold: number;
-    private readonly totalQuantity: number;
+    private readonly productVariations: ProductVariationJson[];
 
     constructor(
         id: number,
         name: string,
-        size: string,
         productTypeId: number,
-        colors: ColorJson[],
-        threshold: number,
-        totalQuantity: number
+        productVariations: ProductVariationJson[]
     ) {
         this.id = id;
         this.name = name;
-        this.size = size;
         this.productTypeId = productTypeId;
-        this.colors = colors;
-        this.threshold = threshold;
-        this.totalQuantity = totalQuantity;
-
+        this.productVariations = productVariations;
     }
 
     public getId(): number {
@@ -36,48 +26,29 @@ export class ProductJson {
         return this.name;
     }
 
-
-    public getSize(): string {
-        return this.size;
-    }
-
     public getProductTypeId(): number {
         return this.productTypeId;
     }
 
-    public getColors(): ColorJson[] {
-        return this.colors;
-    }
-
-    public getThreshold(): number {
-        return this.threshold;
-    }
-
-    public getTotalQuantity(): number {
-        return this.totalQuantity;
+    public getProductVariations(): ProductVariationJson[] {
+        return this.productVariations;
     }
 
     public static fromObject(body: any): ProductJson {
         return new ProductJson(
             Number(body.id),
             body.name,
-            body.size,
             Number(body.productTypeId),
-            body.colors.map(ColorJson.from),
-            Number(body.threshold),
-            Number(body.totalQuantity)
+            body.productVariations.map(ProductVariationJson.fromRequest)
         )
     }
 
-    public static fromObjectAndColor(body: any, colors: ColorJson[]): ProductJson {
+    public static fromObjectAndVariation(body: any, productVariations: ProductVariationJson[]): ProductJson {
         return new ProductJson(
             Number(body.id),
             body.name,
-            body.size,
             Number(body.productTypeId),
-            colors,
-            Number(body.threshold),
-            Number(body.totalQuantity)
+            productVariations
         )
     }
 }
