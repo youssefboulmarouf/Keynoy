30d793a32c6dc254c514f9c1706de9b130e0c0d4
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-04-24 16:35:53 -0400

    remove abstract related code

--- app/src/utilities/BaseService.ts
+++ app/src/utilities/BaseService.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import Logger from "./Logger";
 
-export abstract class BaseService {
+export class BaseService {
     protected prisma: PrismaClient;
     protected readonly logger: Logger;
 
@@ -9,10 +9,4 @@ export abstract class BaseService {
         this.prisma = new PrismaClient();
         this.logger = new Logger(className);
     }
-
-    abstract get(): any;
-    abstract getById(id: number): any;
-    abstract add(entity: any): any;
-    abstract update(id: number, entity: any): any;
-    abstract delete(id: number): void;
 }
