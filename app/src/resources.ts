b54d1a3b2e353b76f5355a366cb395b47114c776
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-04-28 21:39:02 -0400

    added endpoint for company design

--- app/src/resources.ts
+++ app/src/resources.ts
@@ -7,6 +7,7 @@ import ShippingResource from "./shipping/ShippingResource";
 import CompanyResource from "./company/CompanyResource";
 import ColorResource from "./color/ColorResource";
 import ProductVariationResource from "./product/product-variation/ProductVariationResource";
+import CompanyDesignResource from "./company/company-design/CompanyDesignResource";
 
 const router = express.Router();
 
@@ -18,6 +19,8 @@ router.use("/orders", OrderResource);
 router.use("/shipping", ShippingResource);
 router.use("/colors", ColorResource);
 router.use("/expenses", ExpenseResource);
+
+router.use("/company-design", CompanyDesignResource);
 router.use("/companies", CompanyResource);
 
 router.use("/product-variations", ProductVariationResource);
