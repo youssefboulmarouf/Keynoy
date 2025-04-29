38d1bdbe17bc313406fd80e4f095390e73692441
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-04-28 21:40:44 -0400

    added disabled attribute

--- client/src/components/common/TableCallToActionButton.tsx
+++ client/src/components/common/TableCallToActionButton.tsx
@@ -3,13 +3,14 @@ import { Box, Button } from "@mui/material";
 interface TableCallToActionButtonProps {
     callToActionText: string;
     callToActionFunction: () => void;
-    fullwidth: boolean
+    fullwidth: boolean;
+    disabled?: boolean;
 }
 
-const TableCallToActionButton: React.FC<TableCallToActionButtonProps> = ({callToActionText, callToActionFunction, fullwidth}) => {
+const TableCallToActionButton: React.FC<TableCallToActionButtonProps> = ({callToActionText, callToActionFunction, fullwidth, disabled = false}) => {
     return (
         <Box display="flex" gap={1}>
-            <Button fullWidth={fullwidth} variant="contained" color="primary" onClick={() => callToActionFunction()}>
+            <Button fullWidth={fullwidth} variant="contained" color="primary" onClick={callToActionFunction} disabled={disabled}>
                 {callToActionText}
             </Button>
         </Box>
