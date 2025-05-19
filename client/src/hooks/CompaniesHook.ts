a8d24e323faf6c0100076df12927023d670e927c
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-05-18 21:42:26 -0400

    removed unnecessary hooks

--- client/src/hooks/CompaniesHook.ts
+++
@@ -1,9 +0,0 @@
-import {useQuery} from '@tanstack/react-query';
-import {fetchCompanies} from "../api/CompaniesApi";
-
-export const useGetCompaniesHook = () => {
-    return useQuery({
-        queryKey: ['companies'],
-        queryFn: fetchCompanies,
-    });
-};
--- client/src/hooks/ProductsHook.ts
+++
@@ -1,9 +0,0 @@
-import {useQuery} from '@tanstack/react-query';
-import {fetchProducts} from "../api/ProductsApi";
-
-export const useGetProductsHook = () => {
-    return useQuery({
-        queryKey: ['products'],
-        queryFn: fetchProducts,
-    });
-};
