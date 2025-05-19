8f5ea9bffa3567745bc4ba58b87c32770cb17415
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-05-18 21:45:19 -0400

    adjusted ExpenseJson to consider isOrder and isShipping fields

--- app/src/expense/ExpenseJson.ts
+++ app/src/expense/ExpenseJson.ts
@@ -4,19 +4,25 @@ export class ExpenseJson {
     private readonly totalPrice: number;
     private readonly date: Date;
     private readonly orderId: number;
+    private readonly isOrder: boolean;
+    private readonly isShipping: boolean;
 
     constructor(
         id: number,
         name: string,
         totalPrice: number,
         date: Date,
-        orderId: number
+        orderId: number,
+        isOrder: boolean,
+        isShipping: boolean
     ) {
         this.id = id;
         this.name = name;
         this.totalPrice = totalPrice;
         this.date = date;
         this.orderId = orderId;
+        this.isOrder = isOrder;
+        this.isShipping = isShipping
     }
 
     public getId(): number {
@@ -39,13 +45,23 @@ export class ExpenseJson {
         return this.orderId;
     }
 
+    public getIsOrder(): boolean {
+        return this.isOrder;
+    }
+
+    public getIsShipping(): boolean {
+        return this.isShipping;
+    }
+
     public static from(body: any): ExpenseJson {
         return new ExpenseJson(
             Number(body.id),
             body.name,
             Number(body.totalPrice),
             body.date,
-            Number(body.orderId)
+            Number(body.orderId),
+            body.isOrder,
+            body.isShipping
         )
     }
 }
