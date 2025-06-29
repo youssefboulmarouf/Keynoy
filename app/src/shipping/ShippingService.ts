e48fef132ad306669ad8ca239d0429ae33ac002e
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-06-29 19:51:52 -0400

    adjusted shipping logic

--- app/src/shipping/ShippingService.ts
+++ app/src/shipping/ShippingService.ts
@@ -25,7 +25,7 @@ export class ShippingService extends BaseService {
         return data.map(ShippingJson.from);
     };
 
-    async getById(orderId: number): Promise<ShippingJson> {
+    async getByOrderId(orderId: number): Promise<ShippingJson> {
         this.logger.log(`Get shipping details by [orderId:${orderId}]`);
         const data = await this.prisma.shipping.findUnique({
             where: { orderId }
@@ -41,16 +41,17 @@ export class ShippingService extends BaseService {
         const existingOrder = await this.orderService.getById(shippingJson.getOrderId());
 
         BadRequestError.throwIf(
-            existingOrder.getOrderStatus() != OrderStatusEnum.FINISHED,
-            `Order with [status=${existingOrder.getOrderStatus()}] cannot be shipped`
-        );
-
-        BadRequestError.throwIf(
             existingOrder.getOrderType() == OrderTypeEnum.BUY,
             `Order with [type=${existingOrder.getOrderType()}] cannot be shipped`
         );
 
-        await this.updateOrder(existingOrder, OrderStatusEnum.SHIPPED);
+        BadRequestError.throwIf(
+            existingOrder.getOrderStatus() != OrderStatusEnum.FINISHED,
+            `Order with [status=${existingOrder.getOrderStatus()}] cannot be shipped`
+        );
+
+        this.logger.log("Order marked as shipped");
+        await this.updateOrderStatus(existingOrder, OrderStatusEnum.SHIPPED);
 
         this.logger.log(`Delivery entry created`);
         const data = await this.prisma.shipping.create({
@@ -58,33 +59,54 @@ export class ShippingService extends BaseService {
                 orderId: shippingJson.getOrderId(),
                 companyId: shippingJson.getCompanyId(),
                 shippingDate: shippingJson.getShippingDate(),
-                deliveryDate: null,
+                deliveryDate: shippingJson.getDeliveryDate(),
                 price: shippingJson.getPrice()
             }
         });
 
+        if (shippingJson.getDeliveryDate()) {
+            await this.delivered(existingOrder, shippingJson)
+        }
+
         return ShippingJson.from(data);
     };
 
-    async update(orderId: number, shippingJson: ShippingJson): Promise<void> {
+    async update(orderId: number, shippingJson: ShippingJson): Promise<ShippingJson> {
         this.logger.log(`Updating shipping details of order with [id=${shippingJson.getOrderId()}]`);
 
         BadRequestError.throwIf(orderId != shippingJson.getOrderId(), `Shipping id mismatch`);
 
         const existingOrder = await this.orderService.getById(shippingJson.getOrderId());
+
+        BadRequestError.throwIf(
+            existingOrder.getOrderType() == OrderTypeEnum.BUY,
+            `Order with [type=${existingOrder.getOrderType()}] cannot be shipped`
+        );
+
         BadRequestError.throwIf(existingOrder.getOrderStatus() == OrderStatusEnum.DELIVERED, `Order is delivered`);
 
+        BadRequestError.throwIf(
+            existingOrder.getOrderStatus() != OrderStatusEnum.SHIPPED,
+            `Order with [status=${existingOrder.getOrderStatus()}] not be delivered`
+        );
+
         this.logger.log(`Delivery entry updated`);
-        await this.prisma.shipping.update({
+        const data = await this.prisma.shipping.update({
             where: { orderId },
             data: {
                 orderId: shippingJson.getOrderId(),
                 companyId: shippingJson.getCompanyId(),
                 shippingDate: shippingJson.getShippingDate(),
-                deliveryDate: null,
+                deliveryDate: shippingJson.getDeliveryDate(),
                 price: shippingJson.getPrice()
             }
         });
+
+        if (shippingJson.getDeliveryDate()) {
+            await this.delivered(existingOrder, shippingJson)
+        }
+
+        return ShippingJson.from(data);
     }
 
     async delete(orderId: number): Promise<void> {
@@ -97,7 +119,7 @@ export class ShippingService extends BaseService {
         );
 
         this.logger.log(`Update order status to finished`);
-        await this.updateOrder(existingOrder, OrderStatusEnum.FINISHED);
+        await this.updateOrderStatus(existingOrder, OrderStatusEnum.FINISHED);
 
         this.logger.log(`Deleting shipping details`);
         await this.prisma.shipping.delete({
@@ -105,59 +127,40 @@ export class ShippingService extends BaseService {
         });
     }
 
-    async delivered(orderId: number, shippingJson: ShippingJson) {
-        this.logger.log(`Updating delivery details to order with [id=${shippingJson.getOrderId()}]`);
+    async delivered(existingOrder: OrderJson, shippingJson: ShippingJson) {
+        this.logger.log("Order marked as delivered");
+        await this.updateOrderStatus(existingOrder, OrderStatusEnum.DELIVERED);
 
-        BadRequestError.throwIf(orderId != shippingJson.getOrderId(), `Shipping id mismatch`);
+        this.logger.log(`Adding Delivery Expense for SELL order`);
+        await this.expenseService.add(new ExpenseJson(
+            0,
+            `Livraison Commande [${shippingJson.getOrderId()}]`,
+            shippingJson.getPrice(),
+            shippingJson.getDeliveryDate() || new Date(),
+            shippingJson.getOrderId(),
+            false,
+            true
+        ));
 
-        const existingOrder = await this.orderService.getById(shippingJson.getOrderId());
-        BadRequestError.throwIf(
-            existingOrder.getOrderStatus() != OrderStatusEnum.SHIPPED,
-            `Order with [status=${existingOrder.getOrderStatus()}] not be delivered`
-        );
-
-        await this.updateOrder(existingOrder, OrderStatusEnum.DELIVERED);
-
-        const existingShippingDetails = await this.getById(shippingJson.getOrderId());
-
-        this.logger.log(`Delivery entry updated`);
-        await this.prisma.shipping.update({
-            where: { orderId },
-            data: {
-                orderId: existingShippingDetails.getOrderId(),
-                companyId: existingShippingDetails.getCompanyId(),
-                shippingDate: existingShippingDetails.getShippingDate(),
-                deliveryDate: shippingJson.getDeliveryDate(),
-                price: existingShippingDetails.getPrice()
-            }
-        });
-
-        this.logger.log("Shipping marked as delivered");
-
-        if (shippingJson.getPrice() > 0) {
-            this.logger.log(`Adding Delivery Expense for SELL order`);
-            await this.expenseService.add(new ExpenseJson(
-                0,
-                "Order Delivery",
-                existingShippingDetails.getPrice(),
-                shippingJson.getDeliveryDate() || new Date(),
-                existingShippingDetails.getOrderId(),
-                false,
-                true
-            ));
-        }
+        await this.updateOrderExpense(existingOrder);
     }
 
-    private async updateOrder(existingOrder: OrderJson, orderStatus: OrderStatusEnum): Promise<void> {
+    private async updateOrderStatus(existingOrder: OrderJson, orderStatus: OrderStatusEnum): Promise<void> {
         this.logger.log(`Update order status from [${existingOrder.getOrderStatus()}] to [${orderStatus}]`);
         await this.prisma.order.update({
             where: { id: existingOrder.getId() },
             data: {
-                companyId: existingOrder.getCompanyId(),
-                orderType: existingOrder.getOrderType(),
                 orderStatus: orderStatus,
-                totalPrice: existingOrder.getTotalPrice(),
-                date: existingOrder.getDate()
+            }
+        });
+    }
+
+    private async updateOrderExpense(existingOrder: OrderJson): Promise<void> {
+        this.logger.log(`Update order expenses`);
+        await this.prisma.order.update({
+            where: { id: existingOrder.getId() },
+            data: {
+                expenseUpdated: true
             }
         });
     }
