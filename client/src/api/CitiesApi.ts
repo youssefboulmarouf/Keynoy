6b69b138e1824c04127f75223a812cb2fd0cb438
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-07-26 18:36:57 -0400

    added add, update and delete api calls

--- client/src/api/CitiesApi.ts
+++ client/src/api/CitiesApi.ts
@@ -8,3 +8,39 @@ export const fetchCities = async (): Promise<CityJson[]> => {
     }
     return res.json();
 }
+
+export const createCity = async (cityJson: CityJson) => {
+    const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/cities`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(cityJson),
+    });
+    if (!res.ok) {
+        console.log(res);
+        throw new Error('Failed to create city');
+    }
+    return res.json();
+}
+
+export const updateCity = async (cityJson: CityJson) => {
+    const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/cities/${cityJson.id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(cityJson),
+    });
+    if (!res.ok) {
+        console.log(res);
+        throw new Error('Failed to update city');
+    }
+    return res.json();
+}
+
+export const deleteCity = async (cityJson: CityJson) => {
+    const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/cities/${cityJson.id}`, {
+        method: "DELETE",
+    });
+    if (!res.ok) {
+        console.log(res);
+        throw new Error('Failed to delete city');
+    }
+}
