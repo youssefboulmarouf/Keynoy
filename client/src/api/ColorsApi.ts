7f9b6562badb2a3871d996a2c91dedf2cf8952a3
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-07-26 20:08:10 -0400

    added color api calls

--- client/src/api/ColorsApi.ts
+++ client/src/api/ColorsApi.ts
@@ -8,3 +8,39 @@ export const fetchColors = async (): Promise<ColorJson[]> => {
     }
     return res.json();
 }
+
+export const createColor = async (colorJson: ColorJson) => {
+    const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/colors`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(colorJson),
+    });
+    if (!res.ok) {
+        console.log(res);
+        throw new Error('Failed to create color');
+    }
+    return res.json();
+}
+
+export const updateColor = async (colorJson: ColorJson) => {
+    const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/colors/${colorJson.id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(colorJson),
+    });
+    if (!res.ok) {
+        console.log(res);
+        throw new Error('Failed to update color');
+    }
+    return res.json();
+}
+
+export const deleteColor = async (colorJson: ColorJson) => {
+    const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/colors/${colorJson.id}`, {
+        method: "DELETE",
+    });
+    if (!res.ok) {
+        console.log(res);
+        throw new Error('Failed to delete color');
+    }
+}
